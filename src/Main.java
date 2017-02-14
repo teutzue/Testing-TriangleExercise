68eb4cdd5d74c5363df9da00ea7084c98fdd00d3
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-02-14 16:54:03 +0100

    refractoring

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,10 @@
 
-
-import java.util.Scanner;
-
 class Main {
-    private static final Scanner in = new Scanner(System.in);
-
-    private static final String equilateral = "Equilateral";
-    private static final String isosceles = "Isosceles";
-    private static final String scalene = "Scalene";
 
 
     public static void main(String[] args) {
-        System.out.println(triangleType());
-    }
-
-    private static String triangleType(){
-        String input;
-        System.out.println("Insert 3 values separated by coma and press enter: ");
-        input = in.nextLine();
-        if(!input.contains(","))return "Use comas!";
-        String [] numbers = input.split(",");
-
-        int side1 = Integer.parseInt(numbers[0]);
-        int side2 = Integer.parseInt(numbers[1]);
-        int side3 = Integer.parseInt(numbers[2]);
-        if((side1 < 0 || side2 < 0 || side3 < 0)||(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1))
-            return "Value of the sides are invalid";
-        if (numbers.length > 3)return "You entered too many numbers!";
-
-        System.out.println("The numbers are: " + numbers[0] + ',' + numbers[1] +',' + numbers[2]);
-
-        if ((side1 == side2) && (side2== side3)) return equilateral;
-        if (side1 == side2 || side1 == side3 || side2 == side3) return isosceles;
-
-         return scalene;
+        Triangle t = new Triangle();
+        System.out.println(t.triangleType());
     }
 
 }
---
+++ src/Triangle.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * Created by CosticaTeodor on 14/02/2017.
+ */
+public class Triangle {
+    private static final Scanner in = new Scanner(System.in);
+
+    private static final String equilateral = "Equilateral";
+    private static final String isosceles = "Isosceles";
+    private static final String scalene = "Scalene";
+
+    public static String triangleType(){
+        String input;
+        System.out.println("Insert 3 values separated by coma and press enter: ");
+        input = in.nextLine();
+        if(!input.contains(","))return "Use comas!";
+        String [] numbers = input.split(",");
+
+        if(!isNumeric(numbers[0]) || !isNumeric(numbers[1]) || !isNumeric(numbers[2]))
+            return "Input only numbers please!";
+
+        int side1 = Integer.parseInt(numbers[0]);
+        int side2 = Integer.parseInt(numbers[1]);
+        int side3 = Integer.parseInt(numbers[2]);
+        if((side1 < 0 || side2 < 0 || side3 < 0))
+            return "Value of the sides are invalid";
+        if (numbers.length > 3)return "You entered too many numbers!";
+
+        System.out.println("The numbers are: " + numbers[0] + ',' + numbers[1] +',' + numbers[2]);
+
+        if ((side1 == side2) && (side2== side3)) return equilateral;
+        if (side1 == side2 || side1 == side3 || side2 == side3) return isosceles;
+
+        return scalene;
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+}
